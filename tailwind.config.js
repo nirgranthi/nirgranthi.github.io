4f26e0b8548a9f47553990e6113add06ecb2c63f
Nirgranthi <223480893+nirgranthi@users.noreply.github.com>
2026-02-15 14:05:24 +0530

    updated tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-import typography from '@tailwindcss/typography';
-
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    typography,
   ],
 }
